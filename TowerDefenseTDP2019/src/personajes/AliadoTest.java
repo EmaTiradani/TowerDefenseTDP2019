bab181ad58d8fcd3df5ee15f382affb1d11c3b0b
Ema <ematiradani@gmail.com>
2019-09-06 19:13:48 -0300

    Push

--- TowerDefenseTDP2019/src/personajes/AliadoTest.java
+++ TowerDefenseTDP2019/src/personajes/AliadoTest.java
@@ -9,7 +9,7 @@ public class AliadoTest extends Aliado {
 	
 	public AliadoTest(int x, int y) {
 		sprite = new JLabel();
-		sprite.setIcon(new ImageIcon("C:\\Users\\default.LAPTOP-9ASHTB0Q\\Documents\\GitHub\\TowerDefenseTDP2019\\TowerDefenseTDP2019\\src\\GUI\\aliado.png"));
+		sprite.setIcon(new ImageIcon("C:\\Users\\default.LAPTOP-9ASHTB0Q\\Desktop\\marciano.gif"));
 		this.x = x*128;
 		this.y = y*128;
 		sprite.setBounds(this.x, this.y, 128, 128);
